e4557f52d985831597864c6ceff015a95e84dcb3
Cody Martin <12martincody@gmail.com>
2022-09-22 18:53:42 +0000

    Fixed some CSS bugs and designs.

--- client/components/patient-table.jsx
+++ client/components/patient-table.jsx
@@ -22,9 +22,9 @@ export default class PatientTable extends React.Component {
           <thead>
             <tr>
               <th className="col-9 col-md-6 col-lg-3">Patient Name</th>
-              <th className="d-none d-sm-none d-md-table-cell col-lg-4">Exercises</th>
+              <th className="d-none d-sm-none d-md-table-cell col-lg-3">Exercises</th>
               <th className="col-3">Status</th>
-              <th className="d-none d-sm-none d-md-table-cell col-md-3 col-lg-3">Mark as Inactive</th>
+              <th className="d-none d-lg-table-cell col-lg-3">Mark as Inactive</th>
             </tr>
           </thead>
           <tbody>
@@ -53,7 +53,7 @@ function Patient(props) {
       <td>{ name }</td>
       <td className="d-none d-sm-none d-md-table-cell"><i className="fa-solid fa-person-walking ms-4" style={{ transform: 'scaleX(-1)' }}></i></td>
       <td><span className="badge rounded-pill" style={{ backgroundColor: statusColor }}>Active</span></td>
-      <td className="d-none d-sm-none d-md-table-cell"><i className="fa-solid fa-check-to-slot ms-5"></i></td>
+      <td className="d-none d-lg-table-cell"><i className="fa-solid fa-check-to-slot ms-5"></i></td>
     </>
   );
 }
